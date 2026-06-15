51868eb6b85b65a7e7492ea0bacbb2db91794336
ardhikaxx <ardhikayanuar58@gmail.com>
2026-06-15 17:49:56 +0700

    fix: membersihkan kode halaman utama dari modul yang tidak digunakan

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,4 @@
 import Image from "next/image";
-import Link from "next/link";
 import SmoothScrollLink from "./components/SmoothScrollLink";
 import InstagramEmbed from "./components/InstagramEmbed";
 import { embed1, embed2, embed3 } from "./components/InstagramHtml";
